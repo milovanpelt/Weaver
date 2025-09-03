f48459917fdd60c0e8590edf2a4eb2bafa3e5f7d
Milo van Pelt <milovanpelt@gmail.com>
2025-09-03 20:39:25 +0200

    add(Dialogue Container): background color for container & combo box with items

--- WeaverDialogueBuilder/DialogueContainer.cpp
+++ WeaverDialogueBuilder/DialogueContainer.cpp
@@ -6,6 +6,12 @@ DialogueContainer::DialogueContainer(QWidget *parent)
     , ui(new Ui::DialogueContainer)
 {
     ui->setupUi(this);
+
+    int testItemAmount = 35;
+    for (int i = 0; i < testItemAmount; i++)
+    {
+        ui->CharacterList->addItem(QString::number(i) + "- Character Name");
+    }
 }
 
 DialogueContainer::~DialogueContainer()
--- WeaverDialogueBuilder/main.cpp
+++ WeaverDialogueBuilder/main.cpp
@@ -1,11 +1,16 @@
 #include "WeaverWindow.h"
+#include "DialogueContainer.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    WeaverWindow w;
-    w.show();
+
+    WeaverWindow mainWindow;
+    mainWindow.show();
+
+    DialogueContainer dialogueContainer;
+    dialogueContainer.show();
     return a.exec();
 }
