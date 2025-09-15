cf59173befd2b7481a4cc25b6afddf992d3e1cd9
Milo van Pelt <milovanpelt@gmail.com>
2025-09-15 13:00:03 +0200

    fix(DialogueManager.cpp): add namespace to function declarations

--- WeaverLibrary/DialogueManager.cpp
+++ WeaverLibrary/DialogueManager.cpp
@@ -1,30 +1,30 @@
 #include "pch.h"
 #include "DialogueManager.h"
 
-UUIDv4::UUID DialogueManager::AddCharacter(const std::string& name)
+UUIDv4::UUID Weaver::DialogueManager::AddCharacter(const std::string& name)
 {
 	return UUIDv4::UUID();
 }
 
-std::vector<std::string> DialogueManager::GetCharacterNames()
+std::vector<std::string> Weaver::DialogueManager::GetCharacterNames()
 {
 	return std::vector<std::string>();
 }
 
-UUIDv4::UUID DialogueManager::CreateScene(const std::string& name)
+UUIDv4::UUID Weaver::DialogueManager::CreateScene(const std::string& name)
 {
 	return UUIDv4::UUID();
 }
 
-UUIDv4::UUID DialogueManager::CreateDialogueEntry(UUIDv4::UUID sceneID, Weaver::DialogueTypes type)
+UUIDv4::UUID Weaver::DialogueManager::CreateDialogueEntry(UUIDv4::UUID sceneID, Weaver::DialogueTypes type)
 {
 	return UUIDv4::UUID();
 }
 
-void DialogueManager::SetSpeaker(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, UUIDv4::UUID speakerID)
+void Weaver::DialogueManager::SetSpeaker(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, UUIDv4::UUID speakerID)
 {
 }
 
-void DialogueManager::SetLineText(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, const std::string& text)
+void Weaver::DialogueManager::SetLineText(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, const std::string& text)
 {
 }
