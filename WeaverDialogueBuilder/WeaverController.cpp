1e62a4afd3c7ce45f65c7aad511ee31d55865cf1
Milo van Pelt <milovanpelt@gmail.com>
2025-10-22 16:49:30 +0200

    feat(WeaverController): implement functionality for slots to create dialogue, save dialogue, and create a character

--- WeaverDialogueBuilder/WeaverController.cpp
+++ WeaverDialogueBuilder/WeaverController.cpp
@@ -1,14 +1,23 @@
 #include "WeaverController.h"
+#include "FileIO.h"
 
 WeaverController::WeaverController(QObject *parent)
     : QObject{parent}
 {}
 
 void WeaverController::on_SavingDialogueRequested(const std::string& filename)
-{}
+{
+    Weaver::FileIO::SaveDialogueToJSON(filename, Weaver::GetCharacters(), Weaver::GetScenes());
+}
 
 void WeaverController::on_CreateDialogueRequested(UUIDv4::UUID sceneID, UUIDv4::UUID speakerID, Weaver::DialogueTypes type, const std::string& dialogue)
-{}
+{
+    Weaver::AddDialogueToCharacter(sceneID, speakerID, type, dialogue);
+    emit WeaverController::DialogueCreated();
+}
 
 void WeaverController::on_CharacterCreatedRequested(const std::string& name)
-{}
+{
+    Weaver::CreateCharacter(name);
+    emit WeaverController::CharacterCreated(name);
+}
