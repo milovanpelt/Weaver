0d273a5342768a5f707367364d8957183dc2a79a
Milo van Pelt <milovanpelt@gmail.com>
2026-04-27 01:42:35 +0200

    update code

--- Core/WeaverLibrary/Include/DialogueManager.h
+++ Core/WeaverLibrary/Include/DialogueManager.h
@@ -21,19 +21,18 @@ namespace Weaver
 {
 	WEAVERLIBRARY_API UUIDv4::UUID CreateCharacter(const std::string& newCharacterName);
 	WEAVERLIBRARY_API UUIDv4::UUID CreateScene(const std::string& newSceneName);
-	WEAVERLIBRARY_API UUIDv4::UUID AddDialogueToCharacter(UUIDv4::UUID sceneID, UUIDv4::UUID speakerID, Weaver::DialogueTypes type, const std::string& dialogue);
+	WEAVERLIBRARY_API UUIDv4::UUID AddDialogueToCharacter(const UUIDv4::UUID& sceneID, const UUIDv4::UUID& speakerID, Weaver::DialogueTypes type, const std::string& dialogue);
 	
-	WEAVERLIBRARY_API void SetSpeaker(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, UUIDv4::UUID speakerID);
-	WEAVERLIBRARY_API void SetLineText(UUIDv4::UUID sceneID, UUIDv4::UUID lineID, const std::string& text);
+	WEAVERLIBRARY_API void SetSpeaker(const UUIDv4::UUID& sceneID, const UUIDv4::UUID& lineID, const UUIDv4::UUID speakerID);
+	WEAVERLIBRARY_API void SetLineText(const UUIDv4::UUID& sceneID, const UUIDv4::UUID& lineID, const std::string& text);
 
 	WEAVERLIBRARY_API std::vector<std::string> GetCharacterNames();
-	WEAVERLIBRARY_API std::unordered_map<UUIDv4::UUID, Character> GetCharacters();
-	WEAVERLIBRARY_API std::unordered_map<UUIDv4::UUID, Scene> GetScenes();
+	WEAVERLIBRARY_API const std::unordered_map<UUIDv4::UUID, Character>& GetCharacters();
+	WEAVERLIBRARY_API const std::unordered_map<UUIDv4::UUID, Scene>& GetScenes();
 	WEAVERLIBRARY_API UUIDv4::UUID GetSceneIdFromName(const std::string& name);
 	WEAVERLIBRARY_API UUIDv4::UUID GetCharacterIdFromName(const std::string& name);
-	WEAVERLIBRARY_API const std::string GetSceneNameFromID(const UUIDv4::UUID& id);
-	WEAVERLIBRARY_API const std::string GetCharacterNameFromID(const UUIDv4::UUID& id);
-
+	WEAVERLIBRARY_API std::string GetSceneNameFromID(const UUIDv4::UUID& id);
+	WEAVERLIBRARY_API std::string GetCharacterNameFromID(const UUIDv4::UUID& id);
 }
 
 #endif // !DIALOGUEMANAGER_H
